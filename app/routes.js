79c7ef8afff61e7964376067202efb7d878e4b70
Paul Hayes <fofr@users.noreply.github.com>
2015-09-04 12:17:22 +0100

    Start from govuk_prototype_kit

---
+++ app/routes.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bind : function (app) {
+
+    app.get('/', function (req, res) {
+      res.render('index');
+    });
+
+    app.get('/examples/template-data', function (req, res) {
+      res.render('examples/template-data', { 'name' : 'Foo' });
+    });
+
+    // add your routes here
+
+  }
+};
